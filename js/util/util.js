0711dcbe201534d76add4a3b142208e7eceaf197
TK Focht <tkfocht@alum.mit.edu>
2022-02-05 14:55:24 -0800

    Scope by season

--- js/util/util.js
+++ js/util/util.js
@@ -1,3 +1,7 @@
 var csvDateParse = d3.timeParse("%m/%d/%Y");
 var urlDateParse = d3.timeParse("%m-%d-%Y");
 var dateFormat = d3.timeFormat("%m-%d-%Y");
+
+var csvUrlForSeason = function(id) {
+    return "https://docs.google.com/spreadsheets/d/19HyOyYXHlYe_GI_b_gjfjTRF5OrXTH8S25btYGyPgdM/gviz/tq?tqx=out:csv&sheet=" + id;
+};
