5536e08fae406051bb1f67c9efd8bc200144bcc2
TK Focht <tkfocht@alum.mit.edu>
2022-03-26 00:46:24 -0700

    Update to Cloudfront CSV location

--- js/util/util.v2.js
+++ js/util/util.v2.js
@@ -3,5 +3,5 @@ var urlDateParse = d3.timeParse("%m-%d-%Y");
 var dateFormat = d3.timeFormat("%m-%d-%Y");
 
 var csvUrlForSeason = function(id) {
-    return "https://docs.google.com/spreadsheets/d/19HyOyYXHlYe_GI_b_gjfjTRF5OrXTH8S25btYGyPgdM/gviz/tq?tqx=out:csv&sheet=" + id;
+    return "https://j-ometry.com/csvs/" + id + ".csv";
 };
