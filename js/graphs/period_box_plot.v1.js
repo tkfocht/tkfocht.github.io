b766166a5e9534c05738d2a6900e4bac902e0254
TK Focht <tkfocht@alum.mit.edu>
2022-06-11 01:18:24 -0700

    TOC Period summary pages

--- js/graphs/period_box_plot.v1.js
+++ js/graphs/period_box_plot.v1.js
@@ -4,11 +4,11 @@ var renderHighlightContestantBoxPlot = function(
     mappingFunction,
     divId,
     title,
-    dTick,
     filterFunction
 ) {
+    var colorList = d3.schemeCategory10.concat(d3.schemeDark2);
     data = d3.filter(data, filterFunction);
-    var xData = highlightContestants;
+    var xData = highlightContestants.slice();
     var yData = d3.map(xData, cid => d3.map(d3.filter(data, cd => cd['Contestant'] === cid), mappingFunction));
     var dateData = d3.map(xData, cid => d3.map(d3.filter(data, cd => cd['Contestant'] === cid), cd1 => dateFormat(cd1['Date'])));
     xData.push('Others');
@@ -25,12 +25,14 @@ var renderHighlightContestantBoxPlot = function(
             boxpoints: 'all',
             jitter: 0.3,
             whiskerwidth: 0.2,
-            fillcolor: 'cls',
             marker: {
-                size: 3
+                size: 4,
+                color: colorList[i]
             },
+            fillcolor: colorList[i],
             line: {
-                width: 1
+                width: 1.5,
+                color: 'black'
             }
         };
         plotData.push(result);
@@ -42,22 +44,68 @@ var renderHighlightContestantBoxPlot = function(
             autorange: true,
             showgrid: true,
             zeroline: true,
-            dtick: dTick,
-            gridcolor: 'rgb(255, 255, 255)',
             gridwidth: 1,
-            zerolinecolor: 'rgb(255, 255, 255)',
-            zerolinewidth: 2
+            zerolinewidth: 2,
+            zeroline: true,
+            title: title
         },
         margin: {
-            l: 40,
+            l: 60,
             r: 30,
             b: 80,
             t: 100
         },
-        paper_bgcolor: 'rgb(243, 243, 243)',
-        plot_bgcolor: 'rgb(243, 243, 243)',
+        //paper_bgcolor: 'rgb(243, 243, 243)',
+        //plot_bgcolor: 'rgb(243, 243, 243)',
         showlegend: false
     };
 
     Plotly.newPlot(divId, plotData, layout);
 }
+
+var renderHighlightContestantRoundLinePlot = function(
+    data,
+    highlightContestants,
+    mappingFunctionLeft,
+    mappingFunctionRight,
+    divId,
+    title,
+    filterFunction,
+    yAxisLabel
+) {
+    var traces = [];
+    for ( var i = 0; i < highlightContestants.length; i ++ ) {
+        var trace = {
+            x: ['J', 'DJ'],
+            y: [
+                d3.mean(d3.map(d3.filter(d3.filter(data, filterFunction), cd => cd['Contestant'] === highlightContestants[i]), mappingFunctionLeft)),
+                d3.mean(d3.map(d3.filter(d3.filter(data, filterFunction), cd => cd['Contestant'] === highlightContestants[i]), mappingFunctionRight))
+            ],
+            type: 'scatter',
+            name: highlightContestants[i]
+        };
+        traces.push(trace);
+    }
+
+    layout = {
+        title: title,
+        yaxis: {
+            autorange: true,
+            showgrid: true,
+            title: {
+                text: yAxisLabel
+            }
+        },
+        margin: {
+            l: 60,
+            r: 30,
+            b: 80,
+            t: 100
+        },
+        //paper_bgcolor: 'rgb(243, 243, 243)',
+        //plot_bgcolor: 'rgb(243, 243, 243)',
+        showlegend: true
+    };
+
+    Plotly.newPlot(divId, traces, layout);
+};
