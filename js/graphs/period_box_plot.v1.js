74bb9e2e0aedf549c5163389f4cdcc2f9f533c9e
TK Focht <tkfocht@alum.mit.edu>
2022-06-15 01:23:43 -0700

    Fix coloring (add commented out day plots)

--- js/graphs/period_box_plot.v1.js
+++ js/graphs/period_box_plot.v1.js
@@ -63,6 +63,72 @@ var renderHighlightContestantBoxPlot = function(
     Plotly.newPlot(divId, plotData, layout);
 }
 
+var renderDayPodiumContestantBoxPlot = function(
+    data,
+    mappingFunction,
+    divId,
+    title,
+    filterFunction
+) {
+    var colorList = d3.schemeCategory10.concat(d3.schemeDark2);
+    data = d3.filter(data, filterFunction);
+    data = d3.group(data, cd => d3.timeFormat("%A")(cd['Date']) + " " + (cd['Podium'] == 1 ? '1' : '2/3'));
+    var xData = ['Monday 1', 'Tuesday 1', 'Wednesday 1', 'Thursday 1', 'Friday 1',
+        'Monday 2/3', 'Tuesday 2/3', 'Wednesday 2/3', 'Thursday 2/3', 'Friday 2/3'];
+    var yData = d3.map(xData, x => d3.map(data.get(x), mappingFunction));
+    var dateData = d3.map(xData, x => d3.map(data.get(x), cd1 => dateFormat(cd1['Date']) + " " + cd1['Contestant']));
+
+    var plotData = [];
+    for ( var i = 0; i < xData.length; i ++ ) {
+        var result = {
+            type: 'box',
+            y: yData[i],
+            name: xData[i],
+            text: dateData[i],
+            boxpoints: 'all',
+            jitter: 0.3,
+            whiskerwidth: 0.2,
+            marker: {
+                size: 4,
+                color: colorList[i]
+            },
+            fillcolor: colorList[i],
+            line: {
+                width: 1.5,
+                color: 'black'
+            }
+        };
+        plotData.push(result);
+    };
+
+    layout = {
+        title: title,
+        yaxis: {
+            autorange: true,
+            showgrid: true,
+            zeroline: true,
+            gridwidth: 1,
+            zerolinewidth: 2,
+            zeroline: true,
+            title: title
+        },
+        xaxis: {
+            title: 'Day of week and podium'
+        },
+        margin: {
+            l: 60,
+            r: 30,
+            b: 80,
+            t: 100
+        },
+        //paper_bgcolor: 'rgb(243, 243, 243)',
+        //plot_bgcolor: 'rgb(243, 243, 243)',
+        showlegend: false
+    };
+
+    Plotly.newPlot(divId, plotData, layout);
+}
+
 var renderHighlightContestantRoundLinePlot = function(
     data,
     highlightContestants,
@@ -73,6 +139,7 @@ var renderHighlightContestantRoundLinePlot = function(
     filterFunction,
     yAxisLabel
 ) {
+    var colorList = d3.schemeCategory10.concat(d3.schemeDark2);
     var traces = [];
     for ( var i = 0; i < highlightContestants.length; i ++ ) {
         var trace = {
@@ -82,7 +149,10 @@ var renderHighlightContestantRoundLinePlot = function(
                 d3.mean(d3.map(d3.filter(d3.filter(data, filterFunction), cd => cd['Contestant'] === highlightContestants[i]), mappingFunctionRight))
             ],
             type: 'scatter',
-            name: highlightContestants[i]
+            name: highlightContestants[i],
+            marker: {
+                color: colorList[i]
+            }
         };
         traces.push(trace);
     }
